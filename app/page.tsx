890306968a1587672ec96c12d279daa485f82b25
JoseCari <jdnarvaez@unimagdalena.edu.co>
2025-07-09 15:30:02 -0500

    feat/Ahora están mas separado los componentes de la pagina principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,23 @@
-import styles from '@/app/ui/home.module.css'
-
+import { FaReact } from "react-icons/fa";
+import { bebas } from "./ui/fonts";
+import Header from "./components/Header";
 
 const Home = () => {
     return (
-        <div>
-            <h1 className={styles.text}>Comenzando la app!!</h1>
-            <p className='p-10 text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, autem. Officia dolor eum laboriosam repudiandae, sed culpa odit itaque distinctio minus perferendis illo ratione. Voluptate non fugit tempora dolorem recusandae.</p>
-        </div>
+        <main className="flex min-h-screen flex-col">
+            <Header />
+
+            <div className="mt-4 mx-auto flex grow flex-col gap-4 md:flex-row w-4/5">
+            
+                <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+                    <p className={`${bebas.className} text-xl text-gray-800 md:text-3xl md-leading-normal`}>
+                        <strong>Welcome to JosCari</strong> Dashboard en text
+                    </p>
+                </div>
+
+            </div>
+
+        </main>
     );
 };
 
