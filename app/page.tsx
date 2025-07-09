ef4357d002a3274565dad7b3431586ffd6843d7e
JoseCari <jdnarvaez@unimagdalena.edu.co>
2025-07-09 16:10:10 -0500

    feat/lado derecho con imagenes realizado Tener en cuenta que va a tener una imagen diferente cuando este en mobil y cuando esté en computador

--- app/page.tsx
+++ app/page.tsx
@@ -12,14 +12,23 @@ const Home = () => {
             
                 <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
                     <p className={`${bebas.className} text-xl text-gray-800 md:text-3xl md-leading-normal`}>
-                        <strong>Welcome to JosCari</strong> Dashboard en text
+                        <strong>Welcome to JosCari</strong> Dashboard en Next
                     </p>
                     <a href="#" className="flex items-center gap-5 selft-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base">
                         <span>loging</span> <BsArrowRight/>
                     </a>
                 </div>
 
+                {/* Lado derecho de la web */}
+
+                <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+                    <img src="/hero-desktop.png" alt="Screenshots of the dashboard" width={1000} height={700} className="hidden md:block"/>
+                    <img src="/hero-mobile.png" alt="Screenshots of the dashboard mobile" width={560} height={620} className="block md:hidden"/>
+                </div>
+
             </div>
+
+            
 
         </main>
     );
