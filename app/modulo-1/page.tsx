ae55c0b4287c635942c7b4a45f2c3236d55fc344
JoseCari <jdnarvaez@unimagdalena.edu.co>
2025-07-14 16:40:51 -0500

    feat/Ahora se puede ingresar a diferentes page de manera sencilla desde el url

---
+++ app/modulo-1/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Modulo1 =  () => {
+
+    return (
+        <div>
+            <h1>
+                Modulo 1
+            </h1>
+        </div>
+    )
+
+}
+
+export default Modulo1;
---
+++ app/modulo-2/modulo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Modulo2 =  () => {
+
+    return (
+        <div>
+            <h1>
+                Modulo 2
+            </h1>
+        </div>
+    )
+
+}
