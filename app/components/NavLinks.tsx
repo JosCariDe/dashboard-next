f9967449fc5ac54877517bc1cbc35ad0d655b2a7
JoseCari <jdnarvaez@unimagdalena.edu.co>
2025-07-14 21:29:38 -0500

    feat/Creado layout para renderizar todo de con el sidebar de la izquierda y rapido

--- app/components/NavLinks.tsx
+++ app/components/NavLinks.tsx
@@ -28,6 +28,7 @@ const NavLinks = () => {
                     const LinIcon = x.icon;
 
                     return <a
+                    key={x.name}
                         href={x.href} className='flex mx-2 my-1 h-[48px] grow items-center justify-center gap-2 rounded-md bg-slate-500 p-3 text-lg text-white font-bold hover:bg-slate-400 hover:text-white md:flex-none md:justify-start md:p-2 md:px-3'>
                         <LinIcon className='w-6' />
                         <p className='hidden md:block'>{x.name}</p>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,8 @@
 
-import SideNav from '../components/SideNav'
 
 const Dashboard = () => {
   return (
-    <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
-        <aside className='w-full flex-none md:w-64 bg-slate-700'>
-            <SideNav/>
-        </aside>
-        <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>
-            <h1>El contenido</h1>
-        </div>
-    </div>
+    <h1>DashBoard</h1>
   )
 }
 
