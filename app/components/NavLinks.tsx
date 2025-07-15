96f5278b1c174745ca727dbe30fb2b88e731b130
JoseCari <jdnarvaez@unimagdalena.edu.co>
2025-07-14 22:17:39 -0500

    updated/Ahora al seleccionar una opcion de la barra de la izquierda, queda seleccionado visualmente Se instaló tailwind merge para anular problema de clases en tailwind

--- app/components/NavLinks.tsx
+++ app/components/NavLinks.tsx
@@ -1,6 +1,8 @@
+'use client'
 import Link from 'next/link'
-import React from 'react'
 import { FaFileInvoice, FaHome, FaUsers } from 'react-icons/fa'
+import { usePathname } from 'next/navigation'
+import { twMerge } from 'tailwind-merge'
 
 const links = [
     {
@@ -20,8 +22,10 @@ const links = [
     },
 ]
 
-
 const NavLinks = () => {
+
+    const pathName = usePathname();
+
     return (
         <>
             {
@@ -29,8 +33,9 @@ const NavLinks = () => {
                     const LinIcon = x.icon;
 
                     return <Link
-                    key={x.name}
-                        href={x.href} className='flex mx-2 my-1 h-[48px] grow items-center justify-center gap-2 rounded-md bg-slate-500 p-3 text-lg text-white font-bold hover:bg-slate-400 hover:text-white md:flex-none md:justify-start md:p-2 md:px-3'>
+                        key={x.name}
+                        href={x.href} 
+                        className={twMerge('flex mx-2 my-1 h-[48px] grow items-center justify-center gap-2 rounded-md bg-slate-700 p-3 text-lg text-white font-bold hover:bg-slate-500 hover:text-white md:flex-none md:justify-start md:p-2 md:px-3', pathName===x.href && 'bg-slate-500')}>
                         <LinIcon className='w-6' />
                         <p className='hidden md:block'>{x.name}</p>
                     </Link>
